685e5d9749ab9494450bbc0d877e31e7110928e8
Trevor Scherling & Tyler Finley <student@epicodus.com>
2015-09-21 11:23:13 -0700

    pass five specs

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -1,10 +1,36 @@
 # require('pry')
+
 class String
   define_method(:leetspeak) do
-    split_sentence = self.split()
-    split_sentence.each() do |word|
-      word.capitalize()
-    end
-    split_sentence.join(" ")
+
+    sentence = self
+    sentence.gsub(/[eoIs]/, 'e' => 3, 'o' => 0, 'I' => 1, 's' => 'z')
   end
 end
+#     new_word = []
+#     split_sentence = self.split('')
+#     split_sentence.each() do |letter|
+#       if letter == 'e' do
+#        new_word.gsub(/[e]/, '3')
+#       elsif letter ==('o')
+#         new_word.push('0')
+#       elsif letter ==('I')
+#         new_word.push('1')
+#       else
+#         new_word.push(letter)
+#       end
+#     end
+#     new_word.join('')
+#   end
+# end
+
+
+
+
+#     if (split_sentence == 'e') do
+#         split_sentence.gsub(/[e]/, '3')
+#   end
+# end
+#
+#
+# end
--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -6,4 +6,21 @@ describe('String#leetspeak') do
   it('returns a string as is when no leetspeak rules apply') do
     expect(("handy").leetspeak).to(eq("handy"))
   end
+
+  it('replaces every "e" in a string with a "3"') do
+    expect(("elephant").leetspeak).to(eq("3l3phant"))
+  end
+
+  it('replaces every "I" in a string with a "1"') do
+    expect("I like Ike".leetspeak).to(eq("1 lik3 1k3"))
+  end
+
+
+  it('replaces every "o" in a string with a "0"') do
+   expect("moo moo".leetspeak).to(eq("m00 m00"))
+ end
+
+ it('replaces every "s" in a string with a "z"') do
+    expect("roses".leetspeak).to(eq("r0z3z"))
+  end
 end
